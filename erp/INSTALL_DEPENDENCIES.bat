0f02d54608e4e9a0d6ba8568fd13aa51e0f73a7b
BookWide <pmktools@gmail.com>
2026-08-23 23:22:00 +0800

    Delete erp directory

--- erp/INSTALL_DEPENDENCIES.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-chcp 65001 >nul
-python -m pip install --user psycopg2-binary
-pause
--- erp/START_V0.7_POSTGRES_TREE.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting BookWide ERP V0.7...
-python server.py
-pause
